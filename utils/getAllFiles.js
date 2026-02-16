f6e172685959c28fa763f8fa00de23e3baf04917
insanexyz <shreyanshsaha19@proton.me>
2026-02-16 22:11:50 +0530

    Complete getAllFiles.js

---
+++ utils/getAllFiles.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (dir, foldersOnly = false) => {
+  let fileNames = [];
+
+  const files = fs.readdirSync(dir, {withFileTypes: true});
+
+  for (const file of files) {
+    const filePath = path.join(dir, file.name);
+    if (foldersOnly) {
+      if (file.isDirectory()) {
+        fileNames.push(filePath);
+      }
+    } else {
+      fileNames.push(filePath);
+    }
+  }
+
+  console.log(fileNames);
+}
