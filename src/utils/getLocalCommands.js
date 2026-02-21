9c381483db9c9b09bd4cf79ad38270082619eade
insanexyz <shreyanshsaha19@proton.me>
2026-02-21 09:45:15 +0530

    Add getLocalCommand.js which fetches commands of your bot locally

---
+++ src/utils/getLocalCommands.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const getAllFiles = require("./getAllFiles");
+
+module.exports((exceptions = []) => {
+  
+  const commandCategories = getAllFiles(
+    path.join(__dirname, "..", "commands"),
+    true
+  );
+
+  for (const commandCategory of commandCategories) {
+    const commandFiles = getAllFiles(commandCategory);
+
+    for (commandFile of commandFiles) {
+      const commandObject = require(commandFile);
+    }
+
+    // If this command is in exception then dont run and move to next command
+    if (exceptions.includes(commandObject.name)) {
+      continue;
+    }
+
+    localCommands.push(commandObject);
+  }
+
+  return localCommands;
+});
