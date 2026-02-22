4fcfcf9dee8a43bcfb13e7f03704b0b96d921d3c
insanexyz <shreyanshsaha19@proton.me>
2026-02-23 00:13:21 +0530

    Add safegaurds in spam command - cooldowns + delay between messages sent

--- src/commands/misc/spam.js
+++ src/commands/misc/spam.js
@@ -1,4 +1,5 @@
-const { ApplicationCommandOptionType } = require("discord.js");
+const { Client, Interaction, ApplicationCommandOptionType } = require("discord.js");
+const cooldowns = new Set();
 
 module.exports = {
   name: "spam",
@@ -18,9 +19,17 @@ module.exports = {
     }
   ],
 
+  /**
+   * 
+   * @param {Client} client 
+   * @param {Interaction} interaction 
+   * @returns 
+   */
+
   callback: async (client, interaction) => {
     let num = interaction.options.get("count").value;
     let content = interaction.options.get("content").value;
+    let delaySeconds = 5;
 
     if (content.length > 1999) {
       await interaction.reply("Too long message, exceeds 2000 characters");
@@ -28,14 +37,25 @@ module.exports = {
     }
     if (!Number.isInteger(num)) return;
 
+    if (cooldowns.has(interaction.member.id)) {
+      interaction.reply("You can use this command once every 5 mins!\nActual time remaining is not shown.");
+      return;
+    }
+
     if (num > 100) {
       await interaction.reply(`Attempt to spam ${num} times. Count cannot exceeded 100!`);
     }
 
     await interaction.reply(`Spamming "${content}" ${num} times...`);
 
+    cooldowns.add(interaction.member.id);
+    setTimeout(() => {
+      cooldowns.delete(interaction.member.id);
+    }, 60000*5);
+
     for (let i = 0; i < num; i++) {
       await interaction.channel.send(`${content}`);
+      await new Promise(r => setTimeout(r, delaySeconds * 1000));
     }
   }
 }
